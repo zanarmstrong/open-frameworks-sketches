aefec771fb93fa7fb93e27a7433bbf56e839b381
zan <zan.armstrong@gmail.com>
2014-11-23 18:34:44 -0500

    Create ofApp.h

---
+++ reorderGreyPixelsCircle/ofApp.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+        static bool orderPixels(ofVec2f a, ofVec2f b);
+    
+    ofImage baseImage;
+    ofImage finalImage;
+    
+    unsigned char * pixOrig;
+    
+    int width;
+    int height;
+    
+};
