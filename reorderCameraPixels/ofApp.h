080eed3fcf45f9b4e89d7205e2208bf9acb9c27b
zan <zan.armstrong@gmail.com>
2014-11-24 09:56:08 -0500

    Create ofApp.h

---
+++ reorderCameraPixels/ofApp.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+        static bool orderPixels(ofVec2f a, ofVec2f b);
+        static bool reverseOrderPixels(ofVec2f a, ofVec2f b);
+    
+    
+    ofImage finalImage;
+    ofVideoGrabber grabber;
+    vector <ofVec2f> coords;
+    unsigned char * pixFinal;
+    
+    
+    int width;
+    int height;
+    
+};
