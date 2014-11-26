090777e789d792516c54929ea1c484bee7823e4b
zan <zan.armstrong@gmail.com>
2014-11-25 21:17:05 -0500

    Update ofApp.cpp

--- spinning/spinningTriangles/ofApp.cpp
+++ spinning/spinningTriangles/ofApp.cpp
@@ -1,15 +1,7 @@
-//
-//  spiral.cpp
-//  5_sinExample_multiCircles
-//
-//  Created by zan on 11/4/14.
-//
-//
-
-#include "spiral.h"
+#include "ofApp.h"
 
 //--------------------------------------------------------------
-void spiralObject::setup(){
+void ofApp::setup(){
 	
 	ofSetVerticalSync(true);
 	
@@ -17,56 +9,73 @@ void spiralObject::setup(){
 	
 	ofSetCircleResolution(100);
     
-    timeLapsed = 0;
+    spiralObject mySpiral;
+    mySpiral.setOrigin(ofGetWidth()/2, ofGetHeight()/2);
+    
+    spirals.push_back(mySpiral);
+
 }
 
 //--------------------------------------------------------------
-void spiralObject::setOrigin(int x, int y){
-    xOrig = x;
-    yOrig = y;
-}
-
-
-//--------------------------------------------------------------
-void spiralObject::update(){
+void ofApp::update(){
 	
+	
+	
+
 }
 
 //--------------------------------------------------------------
+void ofApp::draw(){
+    for (int i = 0; i < spirals.size(); i++){
+		spirals[i].draw();
+	}
+    
+}
 
-void spiralObject::draw(){
-    
-    float time = 0;
-    if(ofGetElapsedTimef() > 10){
-        time = ofGetElapsedTimef() - 10 - timeLapsed;
-    };
-
-    
-    for (int i = 0; i < 50; i++){
-    
-        float radius = 5 + i * (time * .3);
-        float angle = time * (1 + i / 20.0f);
-        while (angle > TWO_PI) angle -= TWO_PI;
-        
-        float x = xOrig + radius * cos(angle);
-        float y = yOrig + radius * -sin(angle);
-		
-        float colorA = ofMap(angle, 0, TWO_PI, 0, 255);
-        float colorB = ofMap(angle + 5, 0, TWO_PI, 0, 255);
-        
-        ofSetRectMode(OF_RECTMODE_CENTER);
-        ofSetColor(colorA,200,colorB);
-        
-        float sideLength = 7;
-        float angleT = time / 3.0f + i / 4.0f;
-        
-        ofTriangle(x + sideLength * cos(angleT),
-                   y + sideLength * sin(angleT),
-                   x +  sideLength * cos(angleT + 2* pi/3),
-                   y +  sideLength * sin(angleT + 2* pi/3),
-                   x +  sideLength * cos(angleT + 4* pi/3),
-                   y +  sideLength * sin(angleT + 4 * pi/3));
-        
-    }
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
 
 }
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    
+    /*  Include this if you want the center to follow the mouse
+     // if timeLapsed = 0 it doesn't start over, if it equals ofGetElapsedTimef then it does start over
+    timeLapsed = ofGetElapsedTimef();
+    
+    xorig = x;
+    yorig = y;
+     */
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    
+    spiralObject mySpiral;
+    mySpiral.setOrigin(x, y);
+    
+    spirals.push_back(mySpiral);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
